debd5a5a9c98a55392a313a20925767eb72306b9
Kfor <kfor1day@zju.edu.cn>
2019-09-28 17:11:35 +0800

    all

--- server/src/app.js
+++ server/src/app.js
@@ -11,7 +11,7 @@ const path = require('path');
 
 const app = new Koa();
 const router = new Router();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5050;
 
 require('./router')(router);
 
