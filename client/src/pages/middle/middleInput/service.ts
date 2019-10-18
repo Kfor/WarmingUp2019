bc953ffcfcb66cb1c64a37bbf3fe65b10fd0a518
Kfor <kfor1day@zju.edu.cn>
2019-10-18 22:00:42 +0800

    update

--- client/src/pages/middle/middleInput/service.ts
+++ client/src/pages/middle/middleInput/service.ts
@@ -1,17 +1,39 @@
 import request from '@/utils/request';
 
 export async function fakeSubmitForm1(params: any) {
-  return request('/api/middlestream/invest?' + 'userId='+params.userId + '&DInvest='+params.DInvest + '&KInvest='+params.KInvest, {
-    method: 'POST',
-    data: params,
-  });
+  return request(
+    '/api/middlestream/invest?' +
+      'userId=' +
+      params.userId +
+      '&DInvest=' +
+      params.DInvest +
+      '&KInvest=' +
+      params.KInvest,
+    {
+      method: 'POST',
+      data: params,
+    },
+  );
 }
 
 export async function fakeSubmitForm2(params: any) {
-  return request('/api/middlestream/produce?' + 'userId=' + params.userId + '&ka=' + params.ka + '&kb=' + params.kb + '&kc=' + params.kc+ '&amount='+params.amount, {
-    method: 'POST',
-    data: params,
-  });
+  return request(
+    '/api/middlestream/produce?' +
+      'userId=' +
+      params.userId +
+      '&ka=' +
+      params.ka +
+      '&kb=' +
+      params.kb +
+      '&kc=' +
+      params.kc +
+      '&amount=' +
+      params.amount,
+    {
+      method: 'POST',
+      data: params,
+    },
+  );
 }
 
 export async function fakeSubmitForm3(params: any) {
@@ -22,8 +44,11 @@ export async function fakeSubmitForm3(params: any) {
 }
 
 export async function fakeSubmitForm4(params: any) {
-  return request('/api/upstream/repay?' + 'userId=' + params.userId + '&repay=' + params.repay, {
-    method: 'POST',
-    data: params,
-  });
+  return request(
+    '/api/middlestream/repay?' + 'userId=' + params.userId + '&repay=' + params.repay,
+    {
+      method: 'POST',
+      data: params,
+    },
+  );
 }
